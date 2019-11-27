94f916ab60bbeb139714d674a705909ac63d6344
Asbert Ma <stayinalive@msn.cn>
2019-11-27 19:08:13 +0800

    block txs mw

---
+++ middleware/blockTxs.ts
@@ -0,0 +1,9 @@
+import {Context} from '@nuxt/types'
+
+export default async function getBlockTxs(ctx: Context) {
+  try {
+    ctx.payload = await ctx.$axios.$get(`/api/blocks/${ctx.params.id}/transactions`)
+  } catch (error) {
+    console.error(error)
+  }
+}
