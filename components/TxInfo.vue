e6ee52a937d2e7522c377d0ed7610493f1821fd0
Asbert Ma <stayinalive@msn.cn>
2019-11-27 19:29:10 +0800

    key fot symbol

--- components/TxInfo.vue
+++ components/TxInfo.vue
@@ -119,7 +119,7 @@ export default class TxInfo extends Vue {
                 sender: item.sender,
                 recipient: item.recipient,
                 amount: item.amount,
-                symbol: item.token
+                symbol: item.symbol
             }
         })
     }
