eb65714083ac8596286a54f13c57cba658050b62
Asbert Ma <stayinalive@msn.cn>
2019-12-09 15:23:05 +0800

    address to low case

--- components/IdentBox.vue
+++ components/IdentBox.vue
@@ -13,7 +13,7 @@ export default class IdentBox extends Vue {
     get styleObj() {
         if (this.address) {
             return {
-            'background-image': `url('data:image/svg+xml;utf8,${picasso(this.address)}')`,
+            'background-image': `url('data:image/svg+xml;utf8,${picasso(this.address.toLowerCase())}')`,
             'background-size': 'cover',
             'background-repeat': 'no-repeat'
             }
