e034f973f2f43e8f8c888495f81924e07614b5cd
Asbert Ma <stayinalive@msn.cn>
2019-12-10 15:36:42 +0800

    error page

---
+++ pages/error.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="my-3">
+            <h3 class="d-inline-block">
+                <span>Error</span>
+                <span class="text-danger">{{code}}</span>
+            </h3>
+        </div>
+        <div class="text-break">
+            <p class="my-4">
+                This page is not working, please check the error below or contact
+                <a
+                    href="mailto:support@vechain.com"
+                >support@vechain.com</a> if the issue still occurs.
+            </p>
+
+            <p class="text-secondary">{{msg}}</p>
+            <b-button squared variant="outline-primary" :to="{name: 'index'}">BACK TO HOMEPAGE</b-button>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import {Context} from '@nuxt/types'
+@Component({
+    async asyncData(ctx: Context) {
+        return {
+            code: ctx.query.code,
+            msg: ctx.query.msg
+        }
+    }
+})
+export default class ErrorPage extends Vue {
+}
+</script>
--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,10 +1,22 @@
 import { Context } from '@nuxt/types'
+import { AxiosError } from 'axios'
+
+declare module 'axios' {
+  interface AxiosRequestConfig {
+    progress?: boolean
+  }
+}
+
 export default function (ctx: Context) {
-  ctx.$axios.onError(error => {
-    if (error.code === '404') {
-      ctx.redirect('/')
+  ctx.$axios.onResponseError((error: AxiosError) => {
+    if (error.response) {
+      ctx.redirect('/error', {
+        code: error.response.status.toString(),
+        msg: error.response.statusText
+      })
     }
   })
+
   ctx.$axios.onRequest((config: any) => {
     const urls = ['/api/blocks/best', '/api/blocks/recent', '/api/transactions/recent']
     if (urls.indexOf(config.url) >= 0) {
