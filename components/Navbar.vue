c41f3e0d7c0f5cb39d4a154d553611bddb81efd1
Asbert Ma <stayinalive@msn.cn>
2019-12-12 17:15:19 +0800

    nav bar layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,49 +1,83 @@
 <template>
-    <b-navbar toggleable="sm" type="dark" style="background-color: #3C5999">
-        <b-navbar-brand :to="{name: 'index'}">
-            <span class="font-weight-lighter">VeChain</span>
-            <strong>Explorer</strong>
-            <b-badge :class="bclass" class="net-badge text-capitalize">{{network}}</b-badge>
-        </b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav></b-navbar-nav>
-            <b-navbar-nav class="ml-auto mt-3 mt-sm-0 mr-sm-1">
-                <b-nav-form @submit.prevent="onsearch">
-                    <b-input-group>
-                        <b-form-input
-                            size="sm"
-                            v-model.trim="search"
-                            placeholder="Account/TxID/Block#"
-                        ></b-form-input>
-                        <b-input-group-append>
-                            <b-button
+    <div>
+        <b-navbar toggleable="sm" type="dark" class="nav-bar-styles">
+            <b-navbar-brand :to="{name: 'index'}">
+                <span class="font-weight-lighter">VeChain</span>
+                <strong>Explorer</strong>
+                <b-badge :class="bclass" class="net-badge text-capitalize">{{network}}</b-badge>
+            </b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto"></b-navbar-nav>
+                <b-navbar-nav v-if="!jumbotron" class="mt-3 mt-sm-0 mr-sm-1">
+                    <b-nav-form @submit.prevent="onsearch">
+                        <b-input-group>
+                            <b-form-input
                                 size="sm"
-                                class="border-0 shadow-none"
-                                style="background-color: #5A86E6"
-                                type="submit"
-                            >Search</b-button>
-                        </b-input-group-append>
-                    </b-input-group>
-                </b-nav-form>
-            </b-navbar-nav>
-            <b-navbar-nav class="mr-sm-4">
-                <b-nav-item-dropdown toggle-class="text-white" text="Network" right>
-                    <b-dropdown-item
-                        target="_blank"
-                        v-for="item in networks"
-                        :key="item.text"
-                        :href="item.link"
-                    >{{item.text}}</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar>
+                                v-model.trim="search"
+                                placeholder="Account/TxID/Block#"
+                            ></b-form-input>
+                            <b-input-group-append>
+                                <b-button
+                                    size="sm"
+                                    class="border-0 shadow-none"
+                                    style="background-color: #5A86E6"
+                                    type="submit"
+                                >Search</b-button>
+                            </b-input-group-append>
+                        </b-input-group>
+                    </b-nav-form>
+                </b-navbar-nav>
+                <b-navbar-nav>
+                    <b-nav-item-dropdown toggle-class="text-white" text="Network" right>
+                        <b-dropdown-item
+                            target="_blank"
+                            v-for="item in networks"
+                            :key="item.text"
+                            :href="item.link"
+                        >{{item.text}}</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <b-jumbotron v-if="jumbotron" class="nav-bar-styles py-4 rounded-0">
+            <b-container>
+                <b-row class="justify-content-center">
+                    <b-col cols="8" class="pb-2 align-middle d-flex">
+                        <img style="height: 30px; width: 30px" src="~/assets/icon.png" alt="Rounded image" />
+                        <span
+                            class="h4 mb-0 ml-1 font-weight-light text-white"
+                        >What are you looking for?</span>
+                    </b-col>
+                    <b-col cols="8">
+                        <b-form @submit.prevent="onsearch">
+                            <b-input-group>
+                                <b-form-input
+                                    v-model.trim="search"
+                                    placeholder="Search by Account/TxID/Block#"
+                                ></b-form-input>
+                                <b-input-group-append>
+                                    <b-button
+                                        class="border-0 shadow-none"
+                                        style="background-color: #5A86E6"
+                                        type="submit"
+                                    >Search</b-button>
+                                </b-input-group-append>
+                            </b-input-group>
+                        </b-form>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </b-jumbotron>
+    </div>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 @Component
 export default class Navbar extends Vue {
+    @Prop({ default: false })
+    jumbotron!: boolean
+
     network = process.env.current
     search = ''
     bclass = process.env.badgeClass
@@ -62,14 +96,17 @@ export default class Navbar extends Vue {
 }
 </script>
 <style scoped>
+.nav-bar-styles {
+    background-color: #3c5999;
+}
 .net-badge {
     color: #3c5999;
 }
 .net-badge.main {
-    background-color: #FFF;
+    background-color: #fff;
 }
 .net-badge.test {
     background-color: #f3a71a;
-    color: #FFF;
+    color: #fff;
 }
 </style>
