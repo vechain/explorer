e66dd71dd3c3504f5ff000caefeeab20b25af68b
Asbert Ma <stayinalive@msn.cn>
2020-01-02 17:12:00 +0800

    font awewome config

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import {
+    faArrowRight,
+    faAngleUp,
+    faAngleDown,
+    faCopy,
+    faQrcode,
+    faArrowLeft,
+    faExchangeAlt
+} from '@fortawesome/free-solid-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas)
+library.add(
+    faArrowRight,
+    faAngleUp,
+    faAngleDown,
+    faCopy,
+    faQrcode,
+    faArrowLeft,
+    faExchangeAlt
+)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
