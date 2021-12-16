2380609e692c21ffd1c019a5a73e1c2f2a1622dc
AsbertMa <stayinalive@msn.cn>
2021-12-16 17:39:15 +0800

    no more tokens in localStorage

--- plugins/init.ts
+++ plugins/init.ts
@@ -47,15 +47,6 @@ const getLocalAbis = (): { key: string, value: object }[] => {
 }
 
 export default async (ctx: Context, inject: any) => {
-  const tokens = getItem('tokens')
-  if ( !tokens || (tokens && (Date.now() - tokens.time) > 60 * 60 * 1000)) {
-    const list = await ctx.$svc.tokens()
-    setItem('tokens', {
-      time: Date.now(),
-      list: list
-    })
-  }
-  ctx.store.commit('setTokens', getItem('tokens').list)
   const abis = getLocalAbis()
   abis.forEach(item => {
     ctx.store.commit('setAbi', item)
