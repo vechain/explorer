2889a45d292f479dd0ade8bcdf58d25da030e97e
AsbertMa <stayinalive@msn.cn>
2022-11-01 11:55:35 +0800

    block link

--- components/Status.vue
+++ components/Status.vue
@@ -7,17 +7,25 @@
             <b-row no-gutters>
                 <b-col cols="4" class="border-right border-bottom py-2">
                     <div class="text-truncate text-muted small">Best Block</div>
-                    <div class="text-monospace">{{  formatedStatus.best  }}</div>
+                    <div>
+                        <router-link class="text-monospace" :to="{name:'blocks-id', params: {id: formatedStatus.best}}">
+                            {{formatedStatus.best | bNum | numFmt}}
+                        </router-link>
+                    </div>
                 </b-col>
                 <b-col cols="8" class="py-2 pl-2 border-bottom">
-                    <div class="text-truncate text-muted small">Check Point</div>
-                    <div class="text-monospace">{{  formatedStatus.processing  }}</div>
+                    <div class="text-truncate text-muted small">Checkpoint</div>
+                    <div class="text-monospace" :style="{fontSize: formatedStatus.size + 'px'}">{{  formatedStatus.processing  }}</div>
                 </b-col>
             </b-row>
             <b-row no-gutters >
                 <b-col cols="4" class="border-right py-2">
                     <div class="text-truncate text-muted small">Finalized Block</div>
-                    <div class="text-monospace">{{  formatedStatus.finalized  }}</div>
+                    <div>
+                        <router-link class="text-monospace" :to="{name:'blocks-id', params: {id: formatedStatus.finalized}}">
+                            {{formatedStatus.finalized | bNum | numFmt}}
+                        </router-link>
+                    </div>
                 </b-col>
                 <b-col cols="5" class="py-2 pl-2">
                     <div class="text-truncate text-muted small">Scheduled Check point</div>
@@ -33,17 +41,25 @@
             <b-row no-gutters>
                 <b-col cols="6" md="4" class="border-right border-bottom px-1 py-2">
                     <div class="text-truncate text-muted small">Best Block</div>
-                    <div class="text-monospace">{{  formatedStatus.best  }}</div>
+                    <div>
+                        <router-link class="text-monospace" :to="{name:'blocks-id', params: {id: formatedStatus.best}}">
+                            {{formatedStatus.best | bNum | numFmt}}
+                        </router-link>
+                    </div>
                 </b-col>
                 <b-col cols="6" md="4" class="border-bottom pl-2 py-2">
                     <div class="text-truncate text-muted small">Finalized Block</div>
-                    <div class="text-monospace">{{  formatedStatus.finalized  }}</div>
+                    <div>
+                        <router-link class="text-monospace" :to="{name:'blocks-id', params: {id: formatedStatus.finalized}}">
+                            {{formatedStatus.finalized | bNum | numFmt}}
+                        </router-link>
+                    </div>
                 </b-col>
             </b-row>
             <b-row no-gutters>
                 <b-col class="px-1 py-2 border-bottom">
                     <div class="text-truncate text-muted small">Check Point</div>
-                    <div class="text-monospace">{{  formatedStatus.processing  }}</div>
+                    <div class="text-monospace" :style="{fontSize: formatedStatus.size + 'px'}">{{  formatedStatus.processing  }}</div>
                 </b-col>
             </b-row>
             <b-row no-gutters>
@@ -71,14 +87,31 @@ export default class Status extends Vue {
         if (this.infos) {
             const { best, finalized, processing, voting } = this.infos
             const bestNum = Vue.filter('bNum')(best)
-            const finalizedNum = Vue.filter('bNum')(finalized)
+            const processingLen = processing.length
             return {
-                best: bestNum.toLocaleString(),
-                finalized: finalizedNum.toLocaleString(),
-                processing: voting > bestNum ? 'N/A' : processing,
+                best,
+                finalized,
+                processing: voting > bestNum ? 'N/A' : (() => {
+                    if (processing.length === 0) {
+                        return 'N/A'
+                    } else if (processing.length < 4) {
+                        return  processing.map(i => i.toLocaleString()).join(' / ')
+                    } else {
+                        return [...processing.slice(0, 2), '...', processing.at(-1)].join(' / ')
+                    }
+                })(),
                 voting: voting.toLocaleString(),
-                epoch: voting > bestNum ? 'N/A' : (bestNum % 180 + '/180')
+                epoch: voting > bestNum ? 'N/A' : (bestNum % 180 + '/180'),
+                size: (() => {
+                    if (processingLen === 3) {
+                        return 14
+                    } else if (processingLen > 3) {
+                        return 12
+                    }
+                })()
             }
+        } else {
+            return null
         }
     }
 }
