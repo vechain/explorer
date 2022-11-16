3a57af45a52f298a5824dd2159c9df7068e11ab7
tony <liboliqi@gmail.com>
2022-11-16 16:58:02 +0800

    improve blockchain ticker && use ticker in the index page

--- store/plugins/fetchTasks.ts
+++ store/plugins/fetchTasks.ts
@@ -1,11 +1,21 @@
 import { Plugin, Store } from 'vuex/types'
 
+const BLOCK_INTERVAL = 10
 export const fetchBest: Plugin<App.State> = (store: Store<App.State>) => {
   if (process.browser) {
-    setInterval(async () => {
+    const doUpdate = async () => {
       const best: DTO.Best = await store.$svc.best()
       store.commit('setBest', best)
-    }, 10000)
+    }
+    setTimeout(() => {
+      doUpdate().catch()
+      setInterval(async () => {
+        const now = Math.floor(Date.now() / 1000)
+        if (store.state.best && now - store.state.best.timestamp >= BLOCK_INTERVAL) {
+          await doUpdate()
+        }
+      }, 2 * 1000)
+    }, 2 * 1000 - Date.now() % (2 * 1000))
   }
 }
 
